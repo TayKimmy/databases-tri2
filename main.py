31dbe361c2ba4cc5d770ebf0a87a91b2ca3e3245
Tay Kim <taykimpro@gmail.com>
2023-02-07 00:52:46 -0800

    poi

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ from projects.projects import app_projects # Blueprint directory import projects
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
-app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(ranking_api)
 app.register_blueprint(generate_api)
 app.register_blueprint(fact_api)
 app.register_blueprint(schema_api)
+app.register_blueprint(app_projects) # register app pages
+
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
