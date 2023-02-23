190dbe9c8db637cbc9a0df48d8ca9dda8b73ecf9
Tay Kim <taykimpro@gmail.com>
2023-02-23 09:55:40 -0800

    srd

--- main.py
+++ main.py
@@ -70,5 +70,5 @@ def activate_job():
 if __name__ == "__main__":
     # change name for testing
     from flask_cors import CORS
-    cors = CORS(app)
+    cors = CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://firestorm0986.github.io/"}})
     app.run(debug=True, host="0.0.0.0", port="8086")
